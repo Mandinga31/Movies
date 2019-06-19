9436d70aeb6a8b78e3cd975b5fcc4c3eb93eac20
mandinga31 <gonzalo.rozas@gmail.com>
2019-06-19 19:52:21 -0300

    y first commit

---
+++ navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>{{ Titulo }}</h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld v-bind:msg="titulo"/>
+  </div>
+</template> 
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  },
+  data() {
+    return {
+      titulo:"proyecto vue cli"
+    } 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <h1>{{ Titulo }}</h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+  </div>
+</template>
+
+<script>
+import NavBar from './components/Navbar.vue'
+//import content from './components/NavBar.vue'
+
+export default {
+  
+  name: 'app',
+  components: {
+    Navbar
+  },
+  data() {
+    return {
+      titulo:"proyecto vue cli"
+    } 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
